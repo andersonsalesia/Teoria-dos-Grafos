fb73762ed21a313b0b37df353873b8587caf3a46
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2020-10-12 15:20:56 -0300

    Método básico para verificar se grafo é regular

--- src/grafo.py
+++ src/grafo.py
@@ -2,9 +2,20 @@ class Grafo:
   def __init__(self, nome):
     self.nome = nome
     self.vertices = []
+
   def adicionar_vertice(self, vertice):
     self.vertices.append(vertice)
+
   def numero_de_vertices(self):
     return len(self.vertices)
+
   def __repr__(self):
     return f"{self.vertices}"
+  
+  def ehRegular(self) -> bool:
+    primeiro_vertice = self.vertices[0]
+    tamanho_primeiro_vertice = primeiro_vertice.numero_de_arestas()
+    for vertice in self.vertices:
+      if vertice.numero_de_arestas() != tamanho_primeiro_vertice:
+        return False
+    return True
